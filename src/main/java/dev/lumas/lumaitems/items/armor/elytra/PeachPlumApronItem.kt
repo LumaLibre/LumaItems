65f4bb78e2c92a14e54a13251b9a0791309d8fd1
Mitality <96748385+Mitality@users.noreply.github.com>
2026-09-16 19:36:14 +0200

    fix peach plum apron breaking again

--- src/main/java/dev/lumas/lumaitems/items/armor/elytra/PeachPlumApronItem.kt
+++ src/main/java/dev/lumas/lumaitems/items/armor/elytra/PeachPlumApronItem.kt
@@ -49,7 +49,7 @@ class PeachPlumApronItem : CustomItemFunctions() {
         .buildPair()
 
     override fun onPotionEffect(player: Player, event: EntityPotionEffectEvent) {
-        if (event.cause != EntityPotionEffectEvent.Cause.BEACON) {
+        if (event.cause != EntityPotionEffectEvent.Cause.BEACON || player.inventory.chestplate.willBreak(1)) {
             return
         }
 
@@ -64,7 +64,7 @@ class PeachPlumApronItem : CustomItemFunctions() {
             player.playSound(loc, Sound.ITEM_BOTTLE_FILL, 1.0f, 1.0f)
             player.spawnParticle(Particle.INSTANT_EFFECT, loc, 20, 0.4, 0.3, 0.4, PARTICLE_DATA)
 
-            if (random.nextBoolean() && !player.inventory.chestplate.willBreak(1)) {
+            if (random.nextBoolean()) {
                 player.damageItemStack(EquipmentSlot.CHEST, 1)
             }
         }
@@ -72,12 +72,15 @@ class PeachPlumApronItem : CustomItemFunctions() {
         event.isCancelled = true
         player.addPotionEffect(newEffect)
 
+
+        // Is this intentional? - Mit
         if (random.nextBoolean()) {
             player.damageItemStack(EquipmentSlot.CHEST, 1)
         }
     }
 
     override fun onEntityDamage(player: Player, event: EntityDamageByEntityEvent) {
+        if (player.inventory.chestplate.willBreak(1)) return
         val entity = event.entity as? LivingEntity ?: return
         val healAmount = event.damage * 0.25
         val amplifier = (healAmount / 4).toInt() // amplifier is 1 = 4 health
